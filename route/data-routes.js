a1942aba227d527689efeaf872cec306ccb512e1
Jimit Mehta <jimitmehta@MacBook-Pro.local>
2020-06-10 21:11:23 -0700

    added params

--- route/data-routes.js
+++ route/data-routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/api/test", (req, res) => {
+router.post("/api/proof", (req, res) => {
   res.json(req.body);
   console.log(req.body.Name);
+});
+
+router.post("/api/test/:fname", (req, res) => {
+  console.log(req.params);
+  res.json(req.params);
 });
 
 module.exports = router;
